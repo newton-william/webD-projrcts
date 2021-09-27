346c572bb7ab2a6da5849a43305bdcce689ff69f
newton-william <parmeshwarmeena98397@gmail.com>
2021-09-27 16:46:09 +0530

    pluswrk.com

---
+++ JS/learnjs.js
@@ -0,0 +1,51 @@
+// console.log("heallo world !");
+// var a="parmeshwar";
+// console.log(a);
+// var arr=[1,"parmeshwar",3,"meena",5,6];
+// console.log(arr);
+// // learn arthmetic oprator
+// var a=100;
+// var b=10;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// // assignment oprator
+// a+=10000;
+// console.log(a);
+// // comperision oprator
+// var c=89;
+// console.log(a>=c);
+// // logical oprator
+// console.log(true&& true);   
+// console.log(false|| true);
+// console.log(true && false);
+// console.log("not");
+// console.log(!true);
+function avr(a,b) {
+    return (a+b)/2;
+    
+}
+c=avr(10,20);
+c2=avr(30,89);
+console.log(c,c2);
+let arr=[1,2,3,4,5,6];
+// for(let i=0;i<6;i++)
+// {
+//    if(i%2==0){ console.log(arr[i]);}
+// }
+// arr.forEach(function(element)
+// {
+//     console.log(element);
+// });
+var i=0;
+while(i<6)
+{
+    console.log(arr[i])
+    i++;
+}
+i=0;
+do{
+    console.log("one times run hooounga ble kuch bhi ho");
+   
+}while(i>0);
